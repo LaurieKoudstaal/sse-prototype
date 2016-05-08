69b5d71d53a62c22cb08b98f0e7f99b91c048e05
Laurie Koudstaal <laurie.koudstaal@fastmail.com>
2016-05-08 16:19:14 +1000

    Added simple front-end to log messages.
    Fixed backend to be able to access the results
    object.

--- app.js
+++ app.js
@@ -17,37 +17,40 @@ var emitter = new Emitter();
 // Serve up the front-end as static pages
 app.use('/', express.static('ui'));
 
-// create a callback to send the sse:
-callback = function(loadavg) {
-        messageCount++; // Increment our message count
-        res.write('id: ' + messageCount + '\n');
-        res.write("data: " + loadavg + '\n\n'); // Note the extra newline
-    }
+
 
 // Serve the update stream
 app.get('/update-stream', function(req, res) {
-    req.socket.setTimeout(Infinity);
+    req.socket.setTimeout(Number.MAX_SAFE_INTEGER);
     
-    req.socket.setTimeout(Infinity);
-
     res.writeHead(200, {
         'Content-Type': 'text/event-stream',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive'
     });
+    res.write('\n');
+    
+    var messageCount = 0;
 
+    // create a callback to send the sse:
+    callback = function(loadavg) {
+        messageCount++; // Increment our message count
+        res.write('id: ' + messageCount + '\n');
+        res.write("data: " + loadavg + '\n\n'); // Note the extra newline
+    }
     emitter.on('update', callback);
     
-    res.write('\n');
+    
 
     
     req.on("close", function() {
         console.log('Browser closed.');
-        emitter.removeListener(callback);
+        emitter.removeListener('update', callback);
     });
 });
 
 setInterval(function() {
     emitter.emit('update',os.loadavg()[0]);
 }, 5000);
-    
+
+app.listen(3000);
